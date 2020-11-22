2059e80dedce36edbaf57d7382c89d04907f28b2
jcanaviri <jcanaviri20@gmail.com>
2020-11-21 19:09:44 -0600

    Cambios de var a let

--- billete.js
+++ billete.js
@@ -1,56 +1,56 @@
 // La clase billete
 class Billete {
-    constructor(valor, cantidad) {
-        this.imagen = new Image();
-        this.valor = valor;
-        this.cantidad = cantidad;
-        this.imagen.src = imagenes[this.valor];
-    }
+  constructor(valor, cantidad) {
+    this.imagen = new Image();
+    this.valor = valor;
+    this.cantidad = cantidad;
+    this.imagen.src = imagenes[this.valor];
+  }
 }
 
 // Las funciones a utilizar
 const entregarDinero = () => {
-    var t = document.getElementById("dinero");
-    var dinero = parseInt(t.value);
+  var t = document.getElementById("dinero");
+  var dinero = parseInt(t.value);
 
-    for (let i of caja) {
-        if (dinero > 0) {
-            div = Math.floor(dinero / i.valor);
-            papeles = (div > i.cantidad) ? i.cantidad : div;
-            entregado.push(new Billete(i.valor, papeles));
+  for (let i of caja) {
+    if (dinero > 0) {
+      div = Math.floor(dinero / i.valor);
+      papeles = div > i.cantidad ? i.cantidad : div;
+      entregado.push(new Billete(i.valor, papeles));
 
-            dinero -= (i.valor * papeles);
-        }
+      dinero -= i.valor * papeles;
     }
+  }
 
-    validar(dinero);
-}
+  validar(dinero);
+};
 
 const validar = (dinero) => {
-    if (dinero > 0) {
-        resultado.innerHTML = `No hay suficietes fondos`;
-    } else {
-        for (var e of entregado) {
-            if (e.cantidad > 0) {
-                resultado.innerHTML += `<br>${e.cantidad} de billetes $${e.valor}<br>`;
-                for (let i = 0; i < e.cantidad; i++) {
-                    resultado.innerHTML += `<img src="${e.imagen.src}" width="150">`;
-                }
-            }
+  if (dinero > 0) {
+    resultado.innerHTML = `No hay suficietes fondos`;
+  } else {
+    for (var e of entregado) {
+      if (e.cantidad > 0) {
+        resultado.innerHTML += `<br>${e.cantidad} de billetes $${e.valor}<br>`;
+        for (let i = 0; i < e.cantidad; i++) {
+          resultado.innerHTML += `<img src="${e.imagen.src}" width="150">`;
         }
+      }
     }
-}
+  }
+};
 
 const resetear = () => {
-    let dinero = document.getElementById('dinero');
-    dinero.value = '';
-    resultado.innerHTML = '';
+  let dinero = document.getElementById("dinero");
+  dinero.value = "";
+  resultado.innerHTML = "";
 
-    for(let i = 0; i < entregado.length; i++) {
-        entregado[i] = '';
-    }
+  for (let i = 0; i < entregado.length; i++) {
+    entregado[i] = "";
+  }
 
-    console.log(dinero);
+  console.log(dinero);
 
-    dinero.setAttribute("autofocus", "");
-}
+  dinero.setAttribute("autofocus", "");
+};
--- code.js
+++ code.js
@@ -1,20 +1,20 @@
-var imagenes = [];
+let imagenes = [];
 imagenes["10"] = "images/10.jpg";
 imagenes["20"] = "images/20.jpg";
 imagenes["50"] = "images/50.jpg";
 imagenes["100"] = "images/100.jpg";
 
-var caja = [];
-var entregado = [];
+let caja = [];
+let entregado = [];
 caja.push(new Billete(100, 5, imagenes[0]));
 caja.push(new Billete(50, 3, imagenes[1]));
 caja.push(new Billete(20, 2, imagenes[2]));
 caja.push(new Billete(10, 2, imagenes[3]));
 
-var div = 0;
-var papeles = 0;
-var resultado = document.getElementById("resultado");
+let div = 0;
+let papeles = 0;
+let resultado = document.getElementById("resultado");
 
-var b = document.getElementById("extraer");
+let b = document.getElementById("extraer");
 b.addEventListener("click", entregarDinero);
-document.getElementById('resetear').addEventListener('click', resetear);
+document.getElementById("resetear").addEventListener("click", resetear);
